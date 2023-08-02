8c7da2420aaf60978b927f4b72f813571ef58158
Mahmoud <mahmoudabohebil@gmail.com>
2023-08-02 13:44:37 +0300

    Editing inside  SideAppBar.

--- lib/sections/side_appbar_section/side_appbar.dart
+++ lib/sections/side_appbar_section/side_appbar.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio_2/sections/side_appbar_section/component/side_appbar_card.dart';
-
 import '../../models/side_appbar_model.dart';
 
-class SideAppBar extends StatelessWidget {
+class SideAppBar extends StatefulWidget {
   const SideAppBar({super.key});
+
+  @override
+  State<SideAppBar> createState() => _SideAppBarState();
+}
+
+class _SideAppBarState extends State<SideAppBar> {
+  int selectedIndex = 0;
+  int hoverIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +20,52 @@ class SideAppBar extends StatelessWidget {
       child: Column(
         children: List.generate(
           sideAppBarList.length,
-          (index) => SideAppBarCard(index),
+          (index) => buildMenuItem(index),
+        ),
+      ),
+    );
+  }
+
+  Widget buildMenuItem(int index) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      onHover: (value) {
+        setState(() {
+          if (value && selectedIndex != index) {
+            hoverIndex = index;
+          } else {
+            hoverIndex = selectedIndex;
+          }
+        });
+      },
+      child: AnimatedContainer(
+        height: 50,
+        duration: Duration(milliseconds: 300),
+        padding: EdgeInsets.fromLTRB(
+            selectedIndex == index || hoverIndex == index ? 20 : 0, 10, 0, 10),
+        child: Row(
+          children: [
+            Icon(
+              sideAppBarList[index].icon,
+              color: selectedIndex == index ? Colors.black : Color(0xff64646a),
+              size: 18,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              sideAppBarList[index].label,
+              style: TextStyle(
+                color:
+                    selectedIndex == index ? Colors.black : Color(0xff64646a),
+                fontSize: 18,
+              ),
+            )
+          ],
         ),
       ),
     );
