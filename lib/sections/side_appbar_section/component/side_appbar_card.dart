24dcd4d19bcce05faae6ed6974bcfe8dc4faa9d8
Mahmoud <mahmoudabohebil@gmail.com>
2023-08-02 12:49:41 +0300

    Creating SideAppBar section Center Container .

--- lib/sections/side_appbar_section/component/side_appbar_card.dart
+++ lib/sections/side_appbar_section/component/side_appbar_card.dart
@@ -1,18 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_2/models/side_appbar_model.dart';
 
-class SideAppBarCard extends StatelessWidget {
-  final int index;
+// List<bool> list = List.generate(sideAppBarList.length, (index) => false);
 
+class SideAppBarCard extends StatefulWidget {
+  final int index;
   SideAppBarCard(this.index);
+  @override
+  State<SideAppBarCard> createState() => _SideAppBarCardState();
+}
+
+class _SideAppBarCardState extends State<SideAppBarCard> {
+  int selectedIndex = 0;
+  int hoverIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(sideAppBarList[index].icon),
-        Text(sideAppBarList[index].label)
-      ],
+    return InkWell(
+      onTap: () {
+        setState(() {
+          selectedIndex = widget.index;
+        });
+      },
+      onHover: (value) {
+        setState(() {
+          value ? hoverIndex = widget.index : hoverIndex = selectedIndex;
+        });
+      },
+      child: AnimatedContainer(
+        height: 50,
+        duration: Duration(milliseconds: 300),
+        padding: EdgeInsets.fromLTRB(
+            selectedIndex == widget.index ? 20 : 0, 10, 0, 10),
+        child: Row(
+          children: [
+            Icon(
+              sideAppBarList[widget.index].icon,
+              color: Color(0xff64646a),
+              size: 18,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              sideAppBarList[widget.index].label,
+              style: TextStyle(
+                color: selectedIndex == widget.index
+                    ? Colors.black
+                    : Color(0xff64646a),
+                fontSize: 18,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
